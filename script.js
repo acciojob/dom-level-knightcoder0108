5a92489ff784411160a1b9ee92f1ee7317a4699f
knightcoder0108 <ssharma.aabhi@gmail.com>
2024-07-01 21:40:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+//your JS code here. If required.
+
+function findDomElement(elementId) {
+	var element  = document.getElementById(elementId)
+	var level = 0
+
+	while (element) {
+		level++;
+		element = element.parentElement;
+	}
+    console.log(level)
+	return level
+}
+
+var domLevel = findDomElement('level')
+
+alert('The level of the element is: ' + domLevel);
